0b625bd758d74262731b4c22d53638af719e522f
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2020-01-14 15:09:54 +0530

    Added select fields, sorting and pagination facility to the Blogs Api

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,74 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 
 exports.getBlogs = asyncHandler(async (req, res, next) => {
-  const blogs = await Blog.find();
+  let query;
+  const reqQuery = { ...req.query };
 
-  if (!blogs) {
-    return next(new ErrorResponse("Resource was not found!", 404));
+  //fields to use
+  const removeFields = ["select", "sort", "page", "limit"];
+
+  // loop over removeFields and remove from req.query
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // create Query String
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Finding resource
+  query = Blog.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
   }
 
-  res.status(200).json({ success: true, count: blogs.length, data: blogs });
+  // Sort By fields or by creation date
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const skip = (page - 1) * limit;
+  const end = page * limit;
+  const total = await Blog.countDocuments();
+
+  query = query.skip(skip).limit(limit);
+
+  //Executing query
+  const blogs = await query;
+
+  //Pagination Part
+  const pagination = {};
+
+  if (end < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+  if (skip > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
+  //Sending Response
+
+  res.status(200).json({
+    success: true,
+    count: blogs.length,
+    pagination,
+    data: {
+      blogs
+    }
+  });
 });
 
 exports.getBlog = asyncHandler(async (req, res, next) => {
