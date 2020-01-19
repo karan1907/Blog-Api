2dd315a4037563a5132c031dbd698f0f3501c927
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2020-01-19 12:11:13 +0530

    Added protect middleware to avoid unauthorized acess to routes and setup route to get details of logged in users

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,3 +49,13 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie("token", token, options)
     .json({ sucess: true, token });
 };
+
+//Get Current Logged In User
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
